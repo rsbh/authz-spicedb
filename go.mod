af4a026a4b325cf7ce80b7fe8114f8b16c806d13
Rishabh Mishra <me@rsbh.dev>
2021-10-06 17:22:24 +0530

    add read schema and check function

--- go.mod
+++ go.mod
@@ -14,4 +14,17 @@ require (
 	google.golang.org/protobuf v1.27.1 // indirect
 )
 
-require github.com/envoyproxy/protoc-gen-validate v0.6.1 // indirect
+require (
+	github.com/authzed/grpcutil v0.0.0-20210914195113-c0d8369e7e1f
+	github.com/envoyproxy/protoc-gen-validate v0.6.1 // indirect
+	github.com/jzelinskie/stringz v0.0.0-20210414224931-d6a8ce844a70 // indirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
--- main.go
+++ main.go
@@ -1,3 +1,84 @@
+// package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+
+// 	spicedb "github.com/authzed/authzed-go/proto/authzed/api/v0"
+// 	"google.golang.org/grpc"
+// )
+
+// func main() {
+// 	opts := grpc.WithInsecure()
+// 	cc, err := grpc.Dial("localhost:50051", opts)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer cc.Close()
+// 	add_config(cc)
+// 	check_access(cc)
+// 	read_config(cc)
+// }
+
+// func add_config(cc *grpc.ClientConn) {
+// 	client := spicedb.NewNamespaceServiceClient(cc)
+// 	request := &spicedb.WriteConfigRequest{
+// 		Configs: []*spicedb.NamespaceDefinition{
+// 			{
+// 				Name: "default",
+// 				Relation: []*spicedb.Relation{
+// 					{
+// 						Name: "owner",
+// 					},
+// 				},
+// 			},
+// 		},
+// 	}
+// 	resp, err := client.WriteConfig(context.Background(), request)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println(resp)
+// }
+
+// func read_config(cc *grpc.ClientConn) {
+// 	client := spicedb.NewNamespaceServiceClient(cc)
+// 	request := &spicedb.ReadConfigRequest{
+// 		Namespace: "default",
+// 	}
+// 	resp, err := client.ReadConfig(context.Background(), request)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println(resp)
+// }
+
+// func check_access(cc *grpc.ClientConn) {
+// 	client := spicedb.NewACLServiceClient(cc)
+// 	request := &spicedb.CheckRequest{
+// 		TestUserset: &spicedb.ObjectAndRelation{
+// 			ObjectId:  "abcd",
+// 			Relation:  "owner",
+// 			Namespace: "default",
+// 		},
+// 		User: &spicedb.User{
+// 			UserOneof: &spicedb.User_Userset{
+// 				Userset: &spicedb.ObjectAndRelation{
+// 					ObjectId:  "abcd",
+// 					Relation:  "owner",
+// 					Namespace: "default",
+// 				},
+// 			},
+// 		},
+// 	}
+// 	resp, err := client.Check(context.Background(), request)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println(resp)
+// }
+
 package main
 
 import (
@@ -5,63 +86,123 @@ import (
 	"fmt"
 	"log"
 
-	spicedb "github.com/authzed/authzed-go/proto/authzed/api/v0"
+	pbv0 "github.com/authzed/authzed-go/proto/authzed/api/v0"
+	pbv1 "github.com/authzed/authzed-go/proto/authzed/api/v1alpha1"
+	authzedv0 "github.com/authzed/authzed-go/v0"
+	authzedv1 "github.com/authzed/authzed-go/v1alpha1"
 	"google.golang.org/grpc"
 )
 
+const schema = `
+definition blog/user {}
+
+definition blog/post {
+    relation reader: blog/user
+    relation writer: blog/user
+
+    permission read = reader + writer
+    permission write = writer
+}
+
+
+`
+
 func main() {
-	opts := grpc.WithInsecure()
-	cc, err := grpc.Dial("localhost:50051", opts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cc.Close()
-	add_config(cc)
-	check_access(cc)
+	setup()
+	add_permission()
+	check()
 }
 
-func add_config(cc *grpc.ClientConn) {
-	client := spicedb.NewNamespaceServiceClient(cc)
-	request := &spicedb.WriteConfigRequest{
-		Configs: []*spicedb.NamespaceDefinition{
-			{
-				Name: "test",
-				Relation: []*spicedb.Relation{
-					{
-						Name: "user",
-					},
+func setup() {
+	client, err := authzedv1.NewClient(
+		"localhost:50051",
+		// grpcutil.WithBearerToken("t_your_token_here_1234567deadbeef"),
+		// grpcutil.WithSystemCerts(false),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatalf("unable to initialize client: %s", err)
+	}
+
+	request := &pbv1.WriteSchemaRequest{Schema: schema}
+	resp, err := client.WriteSchema(context.Background(), request)
+	if err != nil {
+		log.Fatalf("failed to write schema: %s", err)
+	}
+	fmt.Println("Output", resp)
+}
+
+func add_permission() {
+	client, err := authzedv0.NewClient(
+		"localhost:50051",
+		// grpcutil.WithBearerToken("t_your_token_here_1234567deadbeef"),
+		// grpcutil.WithSystemCerts(false),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatalf("unable to initialize client: %s", err)
+	}
+
+	request := &pbv0.WriteRequest{Updates: []*pbv0.RelationTupleUpdate{
+		{ // Emilia is a Writer on Post 1
+			Operation: pbv0.RelationTupleUpdate_CREATE,
+			Tuple: &pbv0.RelationTuple{
+				User: &pbv0.User{UserOneof: &pbv0.User_Userset{Userset: &pbv0.ObjectAndRelation{
+					Namespace: "blog/user",
+					ObjectId:  "emilia",
+					Relation:  "...",
+				}}},
+				ObjectAndRelation: &pbv0.ObjectAndRelation{
+					Namespace: "blog/post",
+					ObjectId:  "1",
+					Relation:  "writer",
 				},
 			},
 		},
-	}
-	resp, err := client.WriteConfig(context.Background(), request)
+	}}
+
+	resp, err := client.Write(context.Background(), request)
+
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to check permission: %s", err)
 	}
+
 	fmt.Println(resp)
 }
 
-func check_access(cc *grpc.ClientConn) {
-	client := spicedb.NewACLServiceClient(cc)
-	request := &spicedb.CheckRequest{
-		TestUserset: &spicedb.ObjectAndRelation{
-			ObjectId:  "abcd",
-			Relation:  "owner",
-			Namespace: "default",
-		},
-		User: &spicedb.User{
-			UserOneof: &spicedb.User_Userset{
-				Userset: &spicedb.ObjectAndRelation{
-					ObjectId:  "abcd",
-					Relation:  "owner",
-					Namespace: "default",
-				},
-			},
-		},
-	}
-	resp, err := client.Check(context.Background(), request)
+func check() {
+	client, err := authzedv0.NewClient(
+		"localhost:50051",
+		// grpcutil.WithBearerToken("t_your_token_here_1234567deadbeef"),
+		// grpcutil.WithSystemCerts(false),
+		grpc.WithInsecure(),
+	)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("unable to initialize client: %s", err)
 	}
+
+	ctx := context.Background()
+
+	emilia := &pbv0.User{UserOneof: &pbv0.User_Userset{Userset: &pbv0.ObjectAndRelation{
+		Namespace: "blog/post",
+		ObjectId:  "emilia",
+		Relation:  "...",
+	}}}
+
+	// beatrice := &pbv0.User{UserOneof: &pbv0.User_Userset{Userset: &pbv0.ObjectAndRelation{
+	// 		Namespace: "user",
+	// 		ObjectId:  "beatrice",
+	// 		Relation:  "...",
+	// }}}
+
+	post1Reader := &pbv0.ObjectAndRelation{Namespace: "blog/post", ObjectId: "1", Relation: "reader"}
+	// post1Writer := &pbv0.ObjectAndRelation{Namespace: "post", ObjectId: "1", Relation: "writer"}
+
+	resp, err := client.Check(ctx, &pbv0.CheckRequest{User: emilia, TestUserset: post1Reader})
+	if err != nil {
+		log.Fatalf("failed to check permission: %s", err)
+	}
+
 	fmt.Println(resp)
+
 }
