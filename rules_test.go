d48f28f56110525f09213c203ca4c1badbc68f04
Rishabh Mishra <me@rsbh.dev>
2021-10-12 08:18:25 +0530

    tests: add tests for user resource permission

--- rules_test.go
+++ rules_test.go
@@ -9,10 +9,12 @@ import (
 )
 
 func setup() *authz.Authz {
-	return authz.New(
+	a := authz.New(
 		"localhost:50051",
 		grpc.WithInsecure(),
 	)
+	a.Schema.Load()
+	return a
 }
 
 func TestRules(t *testing.T) {
@@ -23,9 +25,24 @@ func TestRules(t *testing.T) {
 		assert.Equal(t, result, true)
 	})
 
-	t.Run("Should return false if user is part of group", func(t *testing.T) {
+	t.Run("Should return false if user is not part of group", func(t *testing.T) {
 		a := setup()
 		result := a.Permission.Check("group:g1#view@user:u2")
 		assert.Equal(t, result, false)
 	})
+
+	t.Run("Should return false if user doesnt have access to resource", func(t *testing.T) {
+		a := setup()
+		result := a.Permission.Check("resource/firehose:f1#manage@user:u2")
+		assert.Equal(t, result, false)
+	})
+
+	t.Run("Should return true if user has access to resource", func(t *testing.T) {
+		a := setup()
+		a.Permission.Add("group:g1#member@user:u1")
+		a.Permission.Add("resource/firehose:f1#manager@group:g1")
+		result := a.Permission.Check("resource/firehose:f1#manage@user:u1")
+		assert.Equal(t, result, true)
+	})
+
 }
